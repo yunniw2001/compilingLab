ac7db3a552517e643e01b46991f28bb1bc8d986e
wuyihang <2025151726@qq.com>
2021-12-21 09:42:38 +0800

    改了除法和注释混淆的bug

--- LAB2.py
+++ LAB2.py
@@ -97,7 +97,7 @@ def lexical_analysis(linelist):
                 judge_number(token)
                 token = ''
             elif word[index] == ';' or word[index] == '(' or word[index] == ')' or word[index] == '}' or word[
-                index] == '{' or word[index] == '-' or word[index] == '+' or word[index] == '/' or word[index] == '*' or \
+                index] == '{' or word[index] == '-' or word[index] == '+' or (word[index] == '/' and not word[index+1] =='/' and not word[index+1] == '*' ) or word[index] == '*' or \
                     word[index] == '%':
                 token = word[index]
                 tokenList.append(token)
@@ -448,13 +448,13 @@ if __name__ == '__main__':
     file = open(input)
     line = file.readline()
     while line:
-        print(line)
-        # if ifNotes and ('*/' not in line):
-        #     line = file.readline()
-        #     continue
-        # lineList = line.split()
-        # lexical_analysis(lineList)
-        # tokenList.append('\n')
+        # print(line)
+        if ifNotes and ('*/' not in line):
+            line = file.readline()
+            continue
+        lineList = line.split()
+        lexical_analysis(lineList)
+        tokenList.append('\n')
         line = file.readline()
     if ifNotes:
         sys.exit(-1)
